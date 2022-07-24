f2f1eb27034b081f36485f2a4819da523ab6f23f
BradyDaniels <danielberbra@gmail.com>
2022-07-24 18:44:49 -0400

    funciones de char a time

--- lib/symetric.c
+++ lib/symetric.c
@@ -41,6 +41,7 @@ int symetric_decript(char *s,char *p){
 int diff_time(time_t start,time_t end,int limit){
 
      double diff_t=difftime(end,start);
+     printf("la diferencia es de %f",diff_t);
      if(diff_t>=limit){
          return -1;
      }
@@ -48,24 +49,80 @@ int diff_time(time_t start,time_t end,int limit){
         return 0;
 }
 
+char *time_to_string(time_t t){
+   char *aux;
+   char s[100];
+   
+   strftime(s,100,"%H:%M:%S",localtime(&t));
+   printf("bandera tal\n");
+   aux=s;
+   return aux;
+
+}
+
+time_t string_to_time(char *s){
+    
+    struct tm ts={0};
+    
+    s[2]=s[5]="\0";
+  
+    ts.tm_hour=atoi(&s[0]);
+    ts.tm_min=atoi(&s[3]);
+    ts.tm_sec=atoi(&s[6]);
+
+    time_t t=mktime(&ts);
+
+    return t;
+
+}
+
+
 
 int main(){
-    /*
+    
     time_t start,end;
+    struct tm tm_end={0},tm_start={0};
+   
+    int rc;
     printf("inicio...\n");
     time(&start);
+    char *s=time_to_string(start);
+    time_t t=string_to_time(s);
+    
+    /*
+    strftime(s,100,"%H:%M:%S",localtime(&start));
+    printf("El tiempo de inicio es: %s \n",s);
+    s[2]=s[5]="\0";
 
-    printf("Espera 5 segundos");
+    tm_end.tm_hour=atoi(&s[0])-1900;
+    tm_end.tm_min=atoi(&s[3])-1;
+    tm_end.tm_sec=atoi(&s[6]);
+
+    time_t t=mktime(&tm_end);
+    */
     sleep(5);
-
     time(&end);
 
-    double diff_t=difftime(end,start);
-    printf("Tiempo de ejecucion: %f\n",diff_t);
-    */
-    symetric_encript("test","admin");
-    symetric_decript("test","admin");
+  
+    /*
+    strftime(s,100,"%H:%M:%S",localtime(&end));
+    printf("El tiempo de fin es: %s \n",s);
 
+    s[2]=s[5]="\0";
+
+    tm_start.tm_hour=atoi(&s[0])-1900;
+    tm_start.tm_min=atoi(&s[3])-1;
+    tm_start.tm_sec=atoi(&s[6]);
+
+    time_t t2=mktime(&tm_start);
+    */
+    diff_time(t,end,1800);
+  
+
+    /*
+      symetric_encript("test","admin");
+      symetric_decript("test","admin");
+    */
     return 0;
 
 }
